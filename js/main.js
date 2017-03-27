bca028559e22870babad4cc751d2807efabe0307
LilyxFiore <liliana.fiorela@gmail.com>
2017-03-27 01:29:22 -0500

    corrigiendo

--- js/main.js
+++ js/main.js
@@ -1,77 +1,58 @@
 function validateForm(){
-  validateName();
-}
-
-function validateName(){
   var name = document.getElementById("name").value;
+  var lastname = document.getElementById("lastname").value;
+  var sel = document.getElementById('bici');
+
+  /** Validacion para nombre**/
   if(name.length==0 || /^\s+|\s+$/.test(name)){
-    alert ("Completar nombre");
+    alert ("Ingrese nombre"); return false;
   }
   else if (/[0-9]/.test(name)){
-    alert("Solo letras");
+    alert("Debe contener solo letras"); return false;
   }
   else if (!(/[^a-z]+([a-z]{1,})/.test(name))){
-    alert("Empezar con mayúscula el nombre");
+    alert("Empezar el nombre con mayúscula"); return false;
   }
-  else{
-    validateLastName();
-  }
-}
 
-function validateLastName(){
-  var lastname = document.getElementById("lastname").value;
+  /** Validacion para apellido**/
   if(lastname.length==0 || /^\s+|\s+$/.test(lastname)){
-    alert ("Completar apellido");
+    alert ("Ingrese apellido"); return false;
   }
   else if (/[0-9]/.test(lastname)){
-    alert("Solo letras");
+    alert("Debe contener solo letras"); return false;
   }
   else if (!(/[^a-z]+([a-z]{1,})/.test(lastname))){
-    alert("Empezar con mayúscula el apellido");
+    alert("Empezar el apellido con mayúscula"); return false;
   }
-  else{
-    validateEmail();
-  }
-}
 
-function validateEmail(){
+  /** Validacion para password**/
   var correo = document.getElementById("input-email").value;
   if(correo.length==0 || /^\s+|\s+$/.test(correo)){
-    alert ("Completar correo");
+    alert ("Ingrese correo"); return false;
   }
   else if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+(\w{2,4})+$/.test(correo))) {
-    alert ("Correo no válido");
+    alert ("Correo no válido. El correo de tener la siguiente estructura correo@dominio.com"); return false;
   }
-  else {
-    validatePassword();
-  }
-}
 
-function validatePassword(){
+  /** Validacion para clave**/
   var clave = document.getElementById("input-password").value;
   if(clave.length==0 || /^\s+|\s+$/.test(clave)){
-    alert ("Completar contraseña");
+    alert ("Completar contraseña");return false;
   }
   else if (clave.length<6) {
-    alert ("Contraseña mínimo 6 carácteres");
+    alert ("Contraseña mínimo 6 carácteres"); return false;
   }
   else if(clave == "123456"){
-    alert("La contraseña debe ser diferente de 123456")
+    alert("La contraseña debe ser diferente de 123456"); return false;
   }
   else if(clave == "098754"){
-    alert("La contraseña debe ser diferente de 098754")
+    alert("La contraseña debe ser diferente de 098754"); return false;
   }
-  else {
-    validateSelection();
-  }
-}
 
-function validateSelection(){
-  var sel = document.getElementById('bici');
+  /* Validacion para el select*/
   var op = sel.options[sel.selectedIndex].text;
   if(sel.value == 0){
-    alert("Eliga una opción");}
-  else {
-    alert ("Usted a elegido: "+ op);
+    alert("Eliga una opción"); return false;
   }
+  return alert("Usted ha sido registrado");
 }
